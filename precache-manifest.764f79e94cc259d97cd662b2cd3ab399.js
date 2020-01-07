ac83b8439aa79606760377ff6e2f3330fedc2f75
ogustm <osward.gust.martinez@gmail.com>
2020-01-07 18:49:16 +0100

    Updates

---
+++ precache-manifest.764f79e94cc259d97cd662b2cd3ab399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d39dac49bead2077a6958b24d62d3c",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "0487c9e4787c01863ac0",
+    "url": "/react-todo-list/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "b01b03ed2cee8eaaa726",
+    "url": "/react-todo-list/static/js/2.cfbc8522.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/react-todo-list/static/js/2.cfbc8522.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0487c9e4787c01863ac0",
+    "url": "/react-todo-list/static/js/main.505547cd.chunk.js"
+  },
+  {
+    "revision": "1da69420ba3592b3ab7f",
+    "url": "/react-todo-list/static/js/runtime-main.3866993a.js"
+  }
+]);
